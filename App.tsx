d106b8820407241a111447ca007f4bee57d132c4
dontyellwillcry <papaporo@proton.me>
2024-01-02 00:05:53 -0600

    Converted the App.js to App.tsx. this app will be written in ts or tsx

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { Button, StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Stuff!</Text>
+      <Text>Converted!!!</Text>
       <Button
         onPress={() => {
           alert('Button Pressed');
